3ad2c3dc4f7fe248c836a4470a9835dc4b574016
Kyle O'Shaughnessy <kyle.oshaughnessy@gmail.com>
2014-06-24 23:02:56 -0600

    made a few changes

--- CameraApp.py
+++ CameraApp.py
@@ -16,33 +16,29 @@ cam.start_preview()
 
 
 def cam_loop(delay):
-    try:
-        for filename in cam.capture_continuous('iamge{counter}.jpg'):
-            print(filename)
-            sleep(delay)
-            tweet_picture(filename)
-            db_upload(filename)
-            email_picture(filename)
-            delete_picture(filename)
+    for filename in cam.capture_continuous('image{counter}.jpg'):
+        print(filename)
+        sleep(delay)
+        tweet_picture(filename)
+        db_upload(filename)
+        email_picture(filename)
+        delete_picture(filename)
 
-    finally:
-        return
-
-def tweet_picture():
+def tweet_picture(filename):
     pass
 
-def db_upload():
+def db_upload(filename):
     pass
 
-def email_picture():
+def email_picture(filename):
     pass
 
-def delete_picture():
+def delete_picture(filename):
     pass
 
 if __name__ == "__main__":
     try:
-        delay = sys.argv[1]
+        delay = int(sys.argv[1])
     except:
         delay = 60
     cam_loop(delay)
